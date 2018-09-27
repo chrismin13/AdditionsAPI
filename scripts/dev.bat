eb40bd0c4e55405bb2f36193ed217a11d48dde64
Christos Miniotis <chrismin1302@gmail.com>
2018-09-27 23:47:55 +0300

    More script changes for 1.13.1

--- scripts/dev.bat
+++ scripts/dev.bat
@@ -1,5 +1,4 @@
 @Echo off
 echo Apply Script: COPY
 echo %1
-echo F|xcopy /y /s /f /q "%1" "C:\Users\chris\Test Servers\Spigot 1.9\plugins\AdditionsAPI.jar"
-echo F|xcopy /y /s /f /q "%1" "C:\Users\chris\Test Servers\Spigot 1.12.2\plugins\AdditionsAPI.jar"
+echo F|xcopy /y /s /f /q "%1" "C:\Users\chris\Test Servers\Spigot 1.13.1\plugins\AdditionsAPI.jar"
